0c51f47fb9470f9bad0178d984bd972904d7318a
Guillem Barba <guillem@nan-tic.com>
2014-06-03 18:36:58 +0200

    PEP8 formatting

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 from trytond.pyson import Eval
 
@@ -9,6 +9,7 @@ __all__ = ['Configuration']
 class Configuration(ModelSingleton, ModelSQL, ModelView):
     'Project Template Configuration'
     __name__ = 'project.template.configuration'
+
     default_template = fields.Property(fields.Many2One('project.work',
             'Default Template', domain=[
                 ('company', 'in', [Eval('context', {}).get('company', 0)]),
